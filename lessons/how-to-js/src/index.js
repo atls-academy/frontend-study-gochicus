a284210e6e076bfde69922d564f204855ae18b3b
gochicus <gochicus@gmail.com>
2020-12-04 19:13:23 +0900

    feat(how-to-js): feat(how-to-js): new code in index.js, readme added
    ISSUES CLOSED: #1

--- lessons/how-to-js/src/index.js
+++ lessons/how-to-js/src/index.js
@@ -1,1 +1,21 @@
-let copyright = 'place some bullshit here'
+let  numberofFilms = +prompt('Сколько фильмов вы просмотрели?', '');
+const personalMoviesDB = {
+  count: numberofFilms,
+  movies: {
+
+  },
+  actors: {
+
+  },
+  geners: [],
+  private:false
+};
+const a =prompt('Последний просмотренный фильм?', '');
+const b =prompt('На сколько оцените его?', '');
+const c =prompt('Последний просмотренный фильм?', '');
+const d =prompt('На сколько оцените его?', '');
+
+personalMoviesDB.movies[a] = b;
+personalMoviesDB.movies[c] = d;
+
+console.log(personalMoviesDB);
