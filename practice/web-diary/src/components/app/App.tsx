bc4f74a175f8cfe1375f7e75261b0a868b6e9080
Aleksandr Zavadkin <gochicus@gmail.com>
2021-03-02 00:12:25 +0900

    feat(web-diary): import and export added
    affects: @practice/web-diary
    
    ISSUES CLOSED: #56

--- practice/web-diary/src/components/app/App.tsx
+++ practice/web-diary/src/components/app/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import AppHeader from '../app-header/AppHeader'
 import SearchPanel from '../search-panel/SearchPanel'
 import PostStatusFilter from '../post-status-filter/PostStatusFilter'
-import PostList from '../PostList/PostList'
+import PostList from '../post-list/PostList'
 import PostAddForm from '../post-add-form/PostAddForm'
 
 import '../../style/app.css'
