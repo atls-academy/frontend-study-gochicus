2e922355c8eef4ac097d8e45b52f0c8528828f9f
Aleksandr Zavadkin <gochicus@gmail.com>
2021-03-01 20:20:05 +0900

    WIP(web-diary): react app basic structure,style,configs
    affects: web-diary
    
    ISSUES CLOSED: #56

---
+++ practice/web-diary/src/components/search-panel/SearchPanel.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import '../../style/search-panel.css'
+
+const SearchPanel = () => {
+  return <input className="form-control search-input" type="text" placeholder="Поиск по записям" />
+}
+
+export default SearchPanel
