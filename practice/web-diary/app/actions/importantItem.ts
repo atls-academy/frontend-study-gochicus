fd3358c73d6bb455a36b6a08df083b7ea36a3b67
Aleksandr Zavadkin <gochicus@gmail.com>
2021-04-28 20:45:23 +0900

    style(web-diary): code style changes
    affects: @web-diary/app, @practice/web-diary
    
    ISSUES CLOSED: #68

--- practice/web-diary/app/actions/importantItem.ts
+++ practice/web-diary/app/actions/importantItem.ts
@@ -1,9 +1,7 @@
 export const importantItem = (postData, id, value) => {
-  const index = postData.findIndex((customElements) => customElements.id === id)
+  const index = postData.findIndex(customElements => customElements.id === id)
   const currentPost = postData[index]
-  let newPost
-  newPost = { ...currentPost, important: value }
-  console.log(newPost.important)
+  const newPost = { ...currentPost, important: value }
 
   return [...postData.slice(0, index), newPost, ...postData.slice(index + 1)]
 }
--- practice/web-diary/app/actions/likeItem.ts
+++ practice/web-diary/app/actions/likeItem.ts
@@ -1,9 +1,7 @@
 export const likeItem = (postData, id, value) => {
-  const index = postData.findIndex((customElements) => customElements.id === id)
+  const index = postData.findIndex(customElements => customElements.id === id)
   const currentPost = postData[index]
-  let newPost
-  newPost = { ...currentPost, like: value }
-  console.log(newPost.like)
+  const newPost = { ...currentPost, like: value }
 
   return [...postData.slice(0, index), newPost, ...postData.slice(index + 1)]
 }
