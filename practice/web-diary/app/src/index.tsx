e92310d541eda1cd6c907cc19ceeecf14024ceac
Aleksandr Zavadkin <gochicus@gmail.com>
2021-05-19 20:01:40 +0900

    refactor(web-diary): render moved to app/src
    affects: @web-diary/app, @practice/web-diary
    
    ISSUES CLOSED: #73

--- practice/web-diary/app/src/index.tsx
+++ practice/web-diary/app/src/index.tsx
@@ -1,3 +1,6 @@
-import App from './App'
+import React    from 'react'
+import ReactDOM from 'react-dom'
 
-export { App }
+import App      from './App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
