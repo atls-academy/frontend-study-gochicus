0a42c29d1a60aa8b8606e8f0aaf39c923cabefeb
Aleksandr Zavadkin <gochicus@gmail.com>
2021-06-08 01:19:31 +0900

    refactor(web-diary): color change logic changed
    ISSUES CLOSED: #73

--- practice/web-diary/fragments/post-сontrols/src/PostControls.tsx
+++ practice/web-diary/fragments/post-сontrols/src/PostControls.tsx
@@ -1,15 +1,15 @@
-import React                        from 'react'
-import { useIntl }                  from 'react-intl'
+import React from 'react'
+import {useIntl} from 'react-intl'
 
-import { Button }                   from '@ui/button'
-import { Input }                    from '@ui/input'
-import { Box, Column, Layout, Row } from '@ui/layout'
-import { Space, Text }              from '@ui/text'
-import { useButtonStatus }          from '@store/button-status'
-import { usePostData }              from '@store/post-data'
-import { useSearchValue }           from '@store/search-status'
+import {Button} from '@ui/button'
+import {Input} from '@ui/input'
+import {Box, Column, Layout, Row} from '@ui/layout'
+import {Space, Text} from '@ui/text'
+import {useButtonStatus} from '@store/button-status'
+import {usePostData} from '@store/post-data'
+import {useSearchValue} from '@store/search-status'
 
-import messages                     from './messages'
+import messages from './messages'
 
 export const PostControls = () => {
   const intl = useIntl()
@@ -29,37 +29,37 @@ export const PostControls = () => {
             }}
           />
         </Box>
-        <Layout flexBasis='8px' />
+        <Layout flexBasis='8px'/>
         <Button
           width='50px'
           color={status === 'all' ? 'white' : 'purple'}
           onClick={() => setStatus('all')}
         >
-          <Text>{intl.formatMessage(messages.all)}</Text>
+          <Text color={status === 'all' ? 'purple' : 'white'}>{intl.formatMessage(messages.all)}</Text>
         </Button>
-        <Layout flexBasis='3px' />
+        <Layout flexBasis='3px'/>
         <Button
           width='50px'
           color={status === 'like' ? 'white' : 'purple'}
           onClick={() => setStatus('like')}
         >
-          <Text>{intl.formatMessage(messages.like)}</Text>
+          <Text color={status === 'like' ? 'purple' : 'white'}>{intl.formatMessage(messages.like)}</Text>
         </Button>
-        <Layout flexBasis='3px' />
+        <Layout flexBasis='3px'/>
         <Button
           width='50px'
           color={status === 'important' ? 'white' : 'purple'}
           onClick={() => setStatus('important')}
         >
-          <Text> {intl.formatMessage(messages.important)}</Text>
+          <Text color={status === 'important' ? 'purple' : 'white'}> {intl.formatMessage(messages.important)}</Text>
         </Button>
       </Row>
-      <Layout flexBasis='25px' />
+      <Layout flexBasis='25px'/>
       <Row justifyContent='space-between'>
         <Box alignItems='center'>
-          <Layout flexBasis='5px' />
+          <Layout flexBasis='5px'/>
           <Text fontSize='small'>{intl.formatMessage(messages.posts)}</Text>
-          <Space />
+          <Space/>
           <Text fontSize='small'>{postData.length}</Text>
         </Box>
         <Box>
