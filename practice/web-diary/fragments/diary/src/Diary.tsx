5796295afd7a418312d3d57730445719ac2198c2
Aleksandr Zavadkin <gochicus@gmail.com>
2021-06-15 23:04:16 +0900

    refactor(web-diary): markup refactored
    affects: @fragments/diary, @practice/web-diary, @ui/list
    
    ISSUES CLOSED: #74

--- practice/web-diary/fragments/diary/src/Diary.tsx
+++ practice/web-diary/fragments/diary/src/Diary.tsx
@@ -9,14 +9,10 @@ import { Box, Column, Layout, Row } from '@ui/layout'
 
 export const Diary = () => {
   return (
-    <Background gradient='blueAndGreenGradient' height='100vh' width={[1, 1, 1]}>
+    <Background gradient='blueAndGreenGradient' height='100vh' width='100%'>
       <Column alignItems='center'>
         <Layout flexBasis={['0px', '50px', '300px']} />
-        <Box
-          backgroundColor='white'
-          width={['100%', '400px', '400px']}
-          height={['100%', '100%', '40%']}
-        >
+        <Box backgroundColor='white' width={['100%', '400px', '400px']}>
           <Row justifyContent='center'>
             <Layout flexBasis='15px' />
             <Column alignItems='center' width='100%'>
@@ -33,6 +29,11 @@ export const Diary = () => {
             <Layout flexBasis='15px' />
           </Row>
         </Box>
+        <Box
+          backgroundColor='white'
+          width={['100%', '400px', '400px']}
+          height={['100%', '0px', '0px']}
+        />
         <Layout flexBasis={['0px', '45px', '145px']} />
       </Column>
     </Background>
