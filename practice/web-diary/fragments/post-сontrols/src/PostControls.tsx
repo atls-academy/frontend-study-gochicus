e60527b4ff2414f04ee13e680818bb8b14e074e1
Aleksandr Zavadkin <gochicus@gmail.com>
2021-06-17 17:20:14 +0900

    style(web-diary): lint changes
    ISSUES CLOSED: #74

--- practice/web-diary/fragments/post-сontrols/src/PostControls.tsx
+++ practice/web-diary/fragments/post-сontrols/src/PostControls.tsx
@@ -18,71 +18,71 @@ export const PostControls = () => {
   const [postData, setPostData] = usePostData()
   return (
     <Box width='100%' justifyContent='center'>
-    <Column width='100%'>
-      <Row justifyContent='center'>
-        <Box width='60%'>
-          <Input
-            size='big'
-            placeholder={intl.formatMessage(messages.search)}
-            onChange={event => {
-              setStatus('search')
-              setSearchValue(event.target.value)
-            }}
-          />
-        </Box>
-        <Layout flexBasis='8px' />
-        <Button
-          width='50px'
-          color={status === 'all' ? 'white' : 'purple'}
-          onClick={() => setStatus('all')}
-        >
-          <Text color={status === 'all' ? 'purple' : 'white'}>
-            {intl.formatMessage(messages.all)}
-          </Text>
-        </Button>
-        <Layout flexBasis='3px' />
-        <Button
-          width='50px'
-          color={status === 'like' ? 'white' : 'purple'}
-          onClick={() => setStatus('like')}
-        >
-          <Text color={status === 'like' ? 'purple' : 'white'}>
-            {intl.formatMessage(messages.like)}
-          </Text>
-        </Button>
-        <Layout flexBasis='3px' />
-        <Button
-          width='50px'
-          color={status === 'important' ? 'white' : 'purple'}
-          onClick={() => setStatus('important')}
-        >
-          <Text color={status === 'important' ? 'purple' : 'white'}>
-            {intl.formatMessage(messages.important)}
-          </Text>
-        </Button>
-      </Row>
-      <Layout flexBasis='25px' />
-      <Row justifyContent='space-between'>
-        <Box alignItems='center'>
-          <Layout flexBasis='5px' />
-          <Text fontSize='small'>{intl.formatMessage(messages.posts)}</Text>
-          <Space />
-          <Text fontSize='small'>{postData.length}</Text>
-        </Box>
-        <Box>
+      <Column width='100%'>
+        <Row justifyContent='center'>
+          <Box width='60%'>
+            <Input
+              size='big'
+              placeholder={intl.formatMessage(messages.search)}
+              onChange={event => {
+                setStatus('search')
+                setSearchValue(event.target.value)
+              }}
+            />
+          </Box>
+          <Layout flexBasis='8px' />
           <Button
-            onClick={() => {
-              setPostData([])
-              setStatus('')
-            }}
+            width='50px'
+            color={status === 'all' ? 'white' : 'purple'}
+            onClick={() => setStatus('all')}
           >
-            <Text padding='5px' color='white' fontSize='small'>
-              {intl.formatMessage(messages.clear)}
+            <Text color={status === 'all' ? 'purple' : 'white'}>
+              {intl.formatMessage(messages.all)}
             </Text>
           </Button>
-        </Box>
-      </Row>
-    </Column>
+          <Layout flexBasis='3px' />
+          <Button
+            width='50px'
+            color={status === 'like' ? 'white' : 'purple'}
+            onClick={() => setStatus('like')}
+          >
+            <Text color={status === 'like' ? 'purple' : 'white'}>
+              {intl.formatMessage(messages.like)}
+            </Text>
+          </Button>
+          <Layout flexBasis='3px' />
+          <Button
+            width='50px'
+            color={status === 'important' ? 'white' : 'purple'}
+            onClick={() => setStatus('important')}
+          >
+            <Text color={status === 'important' ? 'purple' : 'white'}>
+              {intl.formatMessage(messages.important)}
+            </Text>
+          </Button>
+        </Row>
+        <Layout flexBasis='25px' />
+        <Row justifyContent='space-between'>
+          <Box alignItems='center'>
+            <Layout flexBasis='5px' />
+            <Text fontSize='small'>{intl.formatMessage(messages.posts)}</Text>
+            <Space />
+            <Text fontSize='small'>{postData.length}</Text>
+          </Box>
+          <Box>
+            <Button
+              onClick={() => {
+                setPostData([])
+                setStatus('')
+              }}
+            >
+              <Text padding='5px' color='white' fontSize='small'>
+                {intl.formatMessage(messages.clear)}
+              </Text>
+            </Button>
+          </Box>
+        </Row>
+      </Column>
     </Box>
   )
 }
