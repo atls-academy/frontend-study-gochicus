5d3c1942fd2bda02179a1c88650d6ef3b1123d19
Aleksandr Zavadkin <gochicus@gmail.com>
2021-07-01 20:55:24 +0900

    refactor(list): interface changed
    ISSUES CLOSED: #81

--- list/src/List.interface.ts
+++ list/src/List.interface.ts
@@ -1,19 +1,19 @@
 export interface ListInterface {
-  items: [{ post?: string; important?: boolean; like?: boolean; id: number }]
+  items: [{ post?: string | number; important?: boolean; like?: boolean; id: number }]
   deleteItem: (
-    postData: [{ post?: string; important?: boolean; like?: boolean; id: number }],
+    postData: [{ post?: string | number; important?: boolean; like?: boolean; id: number }],
     id: number,
-  ) => []
+  ) => Array<any>
   importantItem: (
-    postData: [{ post?: string; important?: boolean; like?: boolean; id: number }],
+    postData: [{ post?: string | number; important?: boolean; like?: boolean; id: number }],
     id: number,
     status: boolean,
-  ) => []
+  ) => Array<any>
   likeItem: (
-    postData: [{ post?: string; important?: boolean; like?: boolean; id: number }],
+    postData: [{ post?: string | number; important?: boolean; like?: boolean; id: number }],
     id: number,
     status: boolean,
-  ) => []
-  postObject: { post?: string; important?: boolean; like?: boolean; id: number }
-  setPostData: () => []
+  ) => Array<any>
+  postObject: { post?: string | number; important?: boolean; like?: boolean; id: number }
+  setPostData: () => Array<any>
 }
