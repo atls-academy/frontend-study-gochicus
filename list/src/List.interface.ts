464c43716fae91c44821a7e47f7221401e0a285e
Aleksandr Zavadkin <gochicus@gmail.com>
2021-07-01 22:34:51 +0900

    refactor(list): interface changed
    ISSUES CLOSED: #81

--- list/src/List.interface.ts
+++ list/src/List.interface.ts
@@ -1,20 +1,17 @@
 export interface ListInterface {
   items: [PostDataObject['object']]
-  deleteItem: (postData: [PostDataObject['object']], id: number) => [PostDataObject['object']]
-  importantItem: (
-    postData: [PostDataObject['object']],
-    id: number,
-    status: boolean,
-  ) => [PostDataObject['object']]
-  likeItem: (
-    postData: [PostDataObject['object']],
-    id: number,
-    status: boolean,
-  ) => [PostDataObject['object']]
+  deleteItem: (CallbackInterface) => PostDataObject['object'][]
+  importantItem: (CallbackInterface) => PostDataObject['object'][]
+  likeItem: (CallbackInterface) => PostDataObject['object'][]
   postObject: PostDataObject['object']
-  setPostData: () => [PostDataObject['object']]
+  setPostData: () => PostDataObject['object'][]
 }
 
 export interface PostDataObject {
   object: { post?: string | number; important?: boolean; like?: boolean; id: number }
+}
+export interface CallbackInterface {
+  postData?: PostDataObject['object']
+  id: number
+  status?: boolean
 }
