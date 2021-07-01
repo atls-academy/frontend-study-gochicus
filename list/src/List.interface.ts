4bd121baadeab67a1661be548e401cbafb7d572c
Aleksandr Zavadkin <gochicus@gmail.com>
2021-07-01 20:11:43 +0900

    refactor(list): types changed
    ISSUES CLOSED: #81

--- list/src/List.interface.ts
+++ list/src/List.interface.ts
@@ -1,8 +1,19 @@
 export interface ListInterface {
   items: [{ post?: string; important?: boolean; like?: boolean; id: number }]
-  deleteItem: []
-  importantItem: []
-  likeItem: []
+  deleteItem: (
+    postData: [{ post?: string; important?: boolean; like?: boolean; id: number }],
+    id: number,
+  ) => []
+  importantItem: (
+    postData: [{ post?: string; important?: boolean; like?: boolean; id: number }],
+    id: number,
+    status: boolean,
+  ) => []
+  likeItem: (
+    postData: [{ post?: string; important?: boolean; like?: boolean; id: number }],
+    id: number,
+    status: boolean,
+  ) => []
   postObject: { post?: string; important?: boolean; like?: boolean; id: number }
   setPostData: []
 }
