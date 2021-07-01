946ff3d791da56a849fb4f1f5411b352500efb85
Aleksandr Zavadkin <gochicus@gmail.com>
2021-07-01 21:27:56 +0900

    refactor(list): interface changed
    affects: @fragments/post-list, @practice/web-diary
    
    ISSUES CLOSED: #81

--- list/src/List.interface.ts
+++ list/src/List.interface.ts
@@ -3,17 +3,17 @@ export interface ListInterface {
   deleteItem: (
     postData: [{ post?: string | number; important?: boolean; like?: boolean; id: number }],
     id: number,
-  ) => Array<any>
+  ) => [{ post?: string | number; important?: boolean; like?: boolean; id: number }]
   importantItem: (
     postData: [{ post?: string | number; important?: boolean; like?: boolean; id: number }],
     id: number,
     status: boolean,
-  ) => Array<any>
+  ) => [{ post?: string | number; important?: boolean; like?: boolean; id: number }]
   likeItem: (
     postData: [{ post?: string | number; important?: boolean; like?: boolean; id: number }],
     id: number,
     status: boolean,
-  ) => Array<any>
+  ) => [{ post?: string | number; important?: boolean; like?: boolean; id: number }]
   postObject: { post?: string | number; important?: boolean; like?: boolean; id: number }
-  setPostData: () => Array<any>
+  setPostData: () => [{ post?: string | number; important?: boolean; like?: boolean; id: number }]
 }
